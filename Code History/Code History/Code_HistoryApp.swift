5c204fe3e3395a6a5395694171ee9438a489c061
matteastuk <contact@matteast.uk>
2022-06-29 00:46:30 +0100

    update

---
+++ Code History/Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Matthew East on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
